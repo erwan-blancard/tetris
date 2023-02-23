cf2e5624e28b078006aece63dbfc3bded995e225
erwan-blancard <erwan.blancard@laplateforme.io>
2023-02-23 13:57:48 +0100

    Changes by theot

--- playfield.py
+++ playfield.py
@@ -48,8 +48,6 @@ class Playfield:
             self.timer = self.total_time
             self.lines_cleared = 0
 
-        self.rotate = 0
-
         self.blocks: list[list[int]] = []
         for col in range(WIDTH):
             self.blocks.append([])
@@ -134,3 +132,90 @@ class Playfield:
     def get_block_color(self, col, row):
         color_index = self.blocks[col][row]
         return tetromino.get_block_color(color_index)
+
+    def spawn_next_tetrominos(self):
+        self.current_tetromino = self.pending_tetromino
+        self.current_tetromino_pos = (4, -1)
+
+        offset = 0
+        while offset < 3:
+            if not self.current_tetromino.can_rotate(self.blocks, (4, -1 - offset), self.current_tetromino.current_tiles):
+                offset += 1
+            else:
+                break
+        self.current_tetromino_pos = (4, -1 - offset)
+
+        self.pending_tetromino = new_tetromino(self.tilesize)
+
+    def move_tetromino(self, right=False):
+        if self.current_tetromino is not None:
+            if right:
+                if self.current_tetromino.can_move_right(self.blocks, self.current_tetromino_pos):
+                    self.current_tetromino_pos = (self.current_tetromino_pos[0] + 1, self.current_tetromino_pos[1])
+            elif self.current_tetromino.can_move_left(self.blocks, self.current_tetromino_pos):
+                self.current_tetromino_pos = (self.current_tetromino_pos[0] - 1, self.current_tetromino_pos[1])
+
+    def print_tetromino_on_board(self):
+        one_printed = False
+        for col in range(len(self.current_tetromino.get_tiles())):
+            for row in range(len(self.current_tetromino.get_tiles()[0])):
+                if self.current_tetromino.get_tiles()[col][row]:
+                    x = self.current_tetromino_pos[0] + col
+                    y = self.current_tetromino_pos[1] + row
+                    if 0 <= x < len(self.blocks) and 0 <= y < len(self.blocks[0]):
+                        self.blocks[x][y] = self.current_tetromino.color
+                        one_printed = True
+        # stops the game (game over) if no blocks of the tetromino can be printed on the block list
+        if not one_printed:
+            self.stop = True
+
+    def update_TBU(self):
+        if self.turns < 70*3:
+            self.TBU = 0.5 - (self.turns // 70)*0.1
+        else:
+            self.TBU = 0.08
+
+    def update_timer(self):
+        if self.gamemode == SURVIVAL:
+            self.timer = time.time() - self.begining_timer
+        elif self.gamemode == TIME_ATTACK:
+            self.timer = self.total_time - (time.time() - self.begining_timer)
+
+    def fast_fall_tetromino(self):
+        if self.current_tetromino is not None:
+            for i in range(len(self.blocks[0])):
+                if not self.current_tetromino.can_move_down(self.blocks, (self.current_tetromino_pos[0], self.current_tetromino_pos[1]+i)):
+                    self.current_tetromino_pos = (self.current_tetromino_pos[0], self.current_tetromino_pos[1]+i)
+                    self.last_update = 0
+                    break
+
+    def update_lines(self):
+        list_rows_filled = []
+        for row in range(len(self.blocks[0])):
+            row_filled = True
+            for col in range(len(self.blocks)):
+                if self.blocks[col][row] == 0:
+                    row_filled = False
+                    break
+            if row_filled:
+                list_rows_filled.append(row)
+        if len(list_rows_filled) > 0:
+            self.clear_lines(list_rows_filled)
+            self.add_points_by_combo(len(list_rows_filled))
+
+    def clear_lines(self, rows_filled: list[int]):
+        new_blocks: list[list[int]] = []
+        rows_chained = 0
+        for col in range(len(self.blocks)):
+            new_blocks.append([])
+            for row in range(len(self.blocks[0])):
+                new_blocks[col].append(0)
+
+        for row in range(len(self.blocks[0])):
+            if (len(self.blocks[0])-row-1) in rows_filled:
+                rows_chained += 1
+            else:
+                for col in range(len(self.blocks)):
+                    new_blocks[col][(len(self.blocks[0])-row-1)+rows_chained] = self.blocks[col][(len(self.blocks[0])-row-1)]
+
+        self.blocks = new_blocks
